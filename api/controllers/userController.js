5aae87de4197a2a4ad3ca28495c9d4c5264bbe72
gerald <gerryrushway@live.com>
2024-04-20 18:46:09 +0200

    [Feature]: Install prisma and its dependences

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,58 +1,31 @@
-import { v4 as uuidv4 } from "uuid";
-import { pool } from "../utils/postgres.js";
-let users = [];
+// user.controller.js
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-export const createUser = (req, res) => {
-  const user = req.body;
-
-  users.push({ ...user, id: uuidv4() });
-
-  res.send(`User with the username ${user.firstName} was created`);
-};
-
-export const getUsers = (req, res) => {
-  pool.query('SELECT * FROM "User"', (error, results) => {
-    if (error) {
-      console.error("Error fetching users:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    } else {
-      res.status(200).json(results.rows);
-    }
-  });
-};
-
-export const getUser = (req, res) => {
-  const { id } = req.params;
-
-  const foundUser = users.find((user) => user.id === id);
-
-  res.send(foundUser);
-};
-
-export const deleteUser = (req, res) => {
-  const { id } = req.params;
-
-  users = users.filter((user) => user.id !== id);
-
-  res.send(`User with id ${id} deleted from the database`);
-};
-
-export const patchUser = (req, res) => {
-  const { id } = req.params;
-  const { firstName, lastName, age } = req.body;
-  const user = users.find((user) => user.id === id);
-
-  if (firstName) {
-    user.firstName = firstName;
+async function createUser(req, res) {
+  try {
+    const { email, name } = req.body;
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+      },
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
   }
+}
 
-  if (lastName) {
-    user.lastName = lastName;
+async function getUsers(req, res) {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
   }
+}
 
-  if (age) {
-    user.age = age;
-  }
+// Define other controller functions for updating, deleting, etc.
 
-  res.send(`User with the id ${id} has been updated`);
-};
+export { createUser, getUsers };
