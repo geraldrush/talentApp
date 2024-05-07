7df6311c5f3f588895a1418d4d6880b34a81cec9
gerald <gerryrushway@live.com>
2024-05-07 18:04:46 +0200

    [Feature]: Add googleOuth from passport

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import session from "express-session";
+import passport from "passport";
 import bodyParser from "body-parser";
 import usersRoutes from "./routes/users.js";
 import menuRoutes from "./routes/menu.js";
@@ -9,12 +10,36 @@ import notificationRoutes from "./routes/notification.js";
 import orderRoutes from "./routes/order.js";
 import restaurantRoutes from "./routes/restaurant.js";
 import redisStore from "./utils/redis.js";
+import authRoutes from "./routes/auth.js";
+import { __prod__ } from "./utils/constants.js";
 
 const app = express();
 const port = 5000;
 
 // Use bodyParser middleware to parse JSON bodies
 app.use(bodyParser.json());
+
+// Initialize session storage.
+app.use(
+  session({
+    store: redisStore,
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      httpOnly: true,
+      sameSite: "lax",
+      secure: __prod__,
+    },
+  })
+);
+
+// Initialize Passport.js middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Route handlers
 app.use("/users", usersRoutes);
 app.use("/menu", menuRoutes);
 app.use("/categories", categoryRoutes);
@@ -22,16 +47,7 @@ app.use("/favorite", favoriteRoutes);
 app.use("/notifications", notificationRoutes);
 app.use("/orders", orderRoutes);
 app.use("/restaurants", restaurantRoutes);
-
-// Initialize session storage.
-app.use(
-  session({
-    store: redisStore,
-    resave: false, // required: force lightweight session keep alive (touch)
-    saveUninitialized: false, // recommended: only save session when data exists
-    secret: "keyboard cat",
-  })
-);
+app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("<><><> API is running <><><>");
---
+++ api/utils/passport.js
@@ -0,0 +1,75 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import prisma from "./prisma.js";
+
+//extracting email from google authentication
+const extractEmail = (profile) => {
+    if (profile.emails && profiles.emails.length > 0){
+        return profile.emails[0].value;
+        } else {
+            return null;
+        }
+};
+
+const extractImage = (profile) => {
+    if (profile.photos && profile.photos.length > 0 ) {
+        return profile.photos[0].value;
+    }else {
+        return null;
+    }
+}
+
+passport.use(
+    new GoogleStrategy(
+        {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback"
+
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    const email = extractEmail(profile);
+    const image = extractImage(profile);
+
+    try {
+      let userExist = await prisma.user.findUnique({ 
+        where:{
+            googleId: profile.id,
+         },
+    });
+
+      if (!userExist) {
+        const newUser = await prisma.user.create({ 
+          data: {
+            googleId: profile.id,
+            name: profile.displayName,
+            email,
+            image,
+          },
+        });
+        done(null, newUser);
+      } else {
+        done(null, userExist);
+      }
+    } catch (error) {
+      // Handle error
+      console.error(error);
+    }
+  }
+)
+);
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const checkUser = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+    done(null, checkUser);
+  } catch (error) {
+    done(error);
+  }
+});
+
+export default passport;
