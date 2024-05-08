f22516cf3c083d7ac0ecb5877df27caacb81e5f5
gerald <gerryrushway@live.com>
2024-05-08 20:29:22 +0200

    [Feature]: Exclude the session in auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import session from "express-session";
+//import session from "express-session";
 import passport from "passport";
 
 const router = express.Router();
