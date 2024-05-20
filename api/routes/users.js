802d2bf9ed102ca68cd094e1287384ce8404c536
gerald <gerryrushway@live.com>
2024-05-20 21:56:30 +0200

    [Feature]: Add auth guard for in isAuth.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,10 +6,11 @@ import {
   deleteUser,
   updateUser,
 } from "../controllers/userController.js";
+import {isAdmin} from "../utils/isAuth.js";
 
 const router = express.Router();
 
-router.get("/", getUsers);
+router.get("/", isAdmin, getUsers);
 router.post("/", createUser);
 router.get("/:id", getUser);
 router.delete("/:id", deleteUser);
---
+++ api/utils/isAuth.js
@@ -0,0 +1,13 @@
+const checkUserRole = (allowedRole) => async (req, res, next) => {
+    const { user } = req
+
+    if (!user || (user.role && user.role !== allowedRole)) {
+        return res.status(403).json({error: "Unauthorized"});
+    }
+
+    next();
+
+};
+
+export const isAdmin = checkUserRole("ADMIN");
+export const isUser = checkUserRole("USER");
